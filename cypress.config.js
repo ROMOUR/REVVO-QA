9c8352f62d06be1dc8f21a896f0e74658a5aa944
RMOUR <rodolfo_itg@yahoo.com.br>
2025-01-11 11:26:45 -0300

    test: ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 2,
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
   e2e: {
     baseUrl: 'https://sandbox.moodledemo.net/login/index.php',
     setupNodeEvents(on, config) {
